9f0b976a583758d4c93a61f768e58255692d1eaf
BinaeriusTechblog <45320679+BinaeriusTechblog@users.noreply.github.com>
2019-07-26 22:35:42 +0200

    Import file

---
+++ Tabellenausgabe.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "EAN\t\t" << "Article\n";  
+
+	cout << "0885177181222\t" << "Nike Duelist Racer\n"<<"0888507120816\t"<<"Flight One Basketball Shoes\b";
+	
+
+    return 0;
+
+}
+
